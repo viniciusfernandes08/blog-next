aed9f91d9534897806fe6224be773bb15361315d
Vinícius <viniciusfernandesoliveira8@gmail.com>
2025-09-09 21:55:24 -0300

    feat: add components for the posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostHeading } from "@/components/PostHeading";
+import { PostImage } from "@/components/PostImage";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import Image from "next/image";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default function HomePage() {
@@ -12,17 +12,22 @@ export default function HomePage() {
       <Header />
 
       <section className="grid grid-cols-1 gap-6 mb-12 sm:grid-cols-2 group">
-        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
-          <Image
-            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
-            src='/images/bryen_0.png'
-            width={1200}
-            height={720}
-            alt="Título do post" 
-            priority
-          />
-        </Link>
+        <PostImage 
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: '/images/bryen_9.png',
+            alt: 'Imagem pessoa',
+            priority: true,
+          }}
+          
+          linkProps={{
+            href: '#'
+          }}
+        />
+        
         <div className="flex flex-col gap-4 sm:justify-center">
+
           <time 
             dateTime="2025-04-20"
             className="text-slate-600 block text-sm/tight"
@@ -30,9 +35,9 @@ export default function HomePage() {
             20/04/2025 10:00
           </time>
 
-          <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
-            <Link href="#">Lorem ipsun dolor sit amet consectur</Link>
-          </h1>
+          <PostHeading as="h1" url="#">
+            Lorem ipsum dolor sit amet consectur
+          </PostHeading>
 
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,13 +1,47 @@
 import { postRepository } from "@/repositories/post"
+import { PostImage } from "../PostImage"
+import { PostHeading } from "../PostHeading"
 
 export async function PostsList() {
     const posts = await postRepository.findAll()
     
     return (
-      <>
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
        {posts.map((post) => {
-        return <p key={post.id}>{post.title}</p>
+        return ( 
+          <div 
+            className="flex flex-col group gap-4" 
+            key={post.id}>
+              <PostImage 
+                imageProps={{
+                  width: 1200,
+                  height: 720,
+                  src: post.coverImageUrl,
+                  alt: post.title,
+                }}
+                
+                linkProps={{
+                  href: `/post/${post.slug}`
+                }}
+              />
+
+              <div className="flex flex-col gap-4 sm:justify-center">
+                <time 
+                  className="text-slate-600 block text-sm/tight"
+                  dateTime={post.createdAt}
+                >
+                  {post.createdAt}
+                </time>
+      
+                <PostHeading as="h1" url="#">
+                  {post.title}
+                </PostHeading>
+      
+                <p>{post.excerpt}</p>
+              </div>
+          </div>
+        )
        })}
-      </>
+      </div>
     )
 }
