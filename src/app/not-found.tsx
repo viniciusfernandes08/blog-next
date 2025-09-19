9d289e2847025ee40bb3b9adc24a7a967cf6d260
Vinícius <viniciusfernandesoliveira8@gmail.com>
2025-09-18 21:57:51 -0300

    feat: add page and components to handle with errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,11 @@
+import { ErrorMessage } from "@/components/ErrorMessage";
+
 export default function NotFoundPage() {
     return (
-        <div className="flex justify-center items-center min-h-[320px] bg-slate-900 
-        text-slate-100 mb-16 p-8 rounded-xl text-center">
-            <div>
-                <h1 className="text-4xl/tight mg-16 font-extrabold md:text-7xl">
-                  404
-                </h1>
-                <p>
-                    Erro 404 - A página que você tentou acessar não existe
-                </p>
-            </div>
-        </div>
+      <ErrorMessage 
+        pageTitle="Página não encontrada" 
+        contentTitle="404" 
+        content="Erro 404 - A página que você está tentando acessar não existe nesse site" 
+      />
     )
 }
