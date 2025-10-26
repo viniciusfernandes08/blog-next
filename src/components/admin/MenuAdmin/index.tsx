283b8748b58bbce540e06e88276a5b437063e20e
Vinícius <viniciusfernandesoliveira8@gmail.com>
2025-10-26 12:12:40 -0300

    feat: add new funcionalities for menu admin

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,8 +1,19 @@
+'use client'
+
 import clsx from "clsx";
-import { FileTextIcon, HouseIcon } from "lucide-react";
+import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon, PlusIcon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function MenuAdmin() {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const pathname = usePathname() //verifica o path
+
+    useEffect(() => {
+        setIsOpen(false)
+    }, [pathname])
+
     const navClasses = clsx(
         'bg-slate-900 text-slate-100 rounded-lg', 
         'flex flex-col mb-8',
@@ -17,9 +28,31 @@ export function MenuAdmin() {
         'h-10',
         'shrink-0'
     );
-    
+    const openCloseBtnClasses = clsx(
+        linkClasses,
+        'text-blue-200 italic',
+        'sm:hidden'
+    );
+
     return (
         <nav className={navClasses}>
+            <button 
+              className={openCloseBtnClasses} 
+              onClick={() => setIsOpen(prev => !prev)}
+            >
+              {isOpen ? (
+                <>
+                  <CircleXIcon />
+                  Fechar
+                </>
+              ): (
+                <>
+                  <MenuIcon />
+                  Menu
+                </>
+              )}
+            </button>
+            
             <a className={linkClasses} href="/" target="_blank">
               <HouseIcon />
               Home
@@ -28,6 +61,11 @@ export function MenuAdmin() {
             <Link className={linkClasses} href='/admin/post'>
               <FileTextIcon />
               Posts
+            </Link>
+
+            <Link className={linkClasses} href='/admin/post/new'>
+              <PlusIcon />
+              Criar post
             </Link>
         </nav>
     )
