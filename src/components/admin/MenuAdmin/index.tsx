1b4865af012f6b4b863340d4463f367ed6151317
Vinícius <viniciusfernandesoliveira8@gmail.com>
2025-10-26 11:43:28 -0300

    feat: add clsx and styles for menu admin

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,11 +1,34 @@
+import clsx from "clsx";
+import { FileTextIcon, HouseIcon } from "lucide-react";
 import Link from "next/link";
 
 export function MenuAdmin() {
+    const navClasses = clsx(
+        'bg-slate-900 text-slate-100 rounded-lg', 
+        'flex flex-col mb-8',
+        'sm:flex-row sm:flex-wrap',
+        //'h-10',
+        //'overflow-hidden',
+    );
+    const linkClasses = clsx(
+        '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
+        'flex items-center justify-start gap-2',
+        'transition hover:bg-slate-800 rounded-lg',
+        'h-10',
+        'shrink-0'
+    );
+    
     return (
-        <nav>
-            <a href="/" target="_blank">Home</a>
+        <nav className={navClasses}>
+            <a className={linkClasses} href="/" target="_blank">
+              <HouseIcon />
+              Home
+            </a>
 
-            <Link href='/admin/post'>Posts</Link>
+            <Link className={linkClasses} href='/admin/post'>
+              <FileTextIcon />
+              Posts
+            </Link>
         </nav>
     )
 }
