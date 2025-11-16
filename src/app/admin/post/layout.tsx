9bcb83bd71eb14ec3eaac39373e3d66bdfcf8baa
Vinícius <viniciusfernandesoliveira8@gmail.com>
2025-11-16 11:45:55 -0300

    feat: update login and logout action, and use them on page and components

--- src/app/admin/post/layout.tsx
+++ src/app/admin/post/layout.tsx
@@ -1,14 +1,16 @@
 import { MenuAdmin } from "@/components/admin/MenuAdmin"
+import { requireLoginSessionOrRedirect } from "@/lib/login/manage-login"
 
 type Props = {
     children: React.ReactNode
 }
 
-export default function AdminPostLayout({children}: Props) {
+export default async function AdminPostLayout({children}: Props) {
+    await requireLoginSessionOrRedirect()
+    
     return (
         <>
           <MenuAdmin />
-
           {children}
         </>
     )
--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,14 +1,16 @@
 'use client'
 
+import { logoutAction } from "@/actions/login/logout-action";
 import clsx from "clsx";
-import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon, PlusIcon } from "lucide-react";
+import { CircleXIcon, FileTextIcon, HourglassIcon, HouseIcon, LogOutIcon, MenuIcon, PlusIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 
 export function MenuAdmin() {
     const [isOpen, setIsOpen] = useState<boolean>(false)
     const pathname = usePathname() //verifica o path
+    const [isPending, startTransition] = useTransition()
 
     useEffect(() => {
         setIsOpen(false)
@@ -33,6 +35,14 @@ export function MenuAdmin() {
         'text-blue-200 italic',
         'sm:hidden'
     );
+
+    function handleLogout(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+      e.preventDefault()
+
+      startTransition(async() => {
+        await logoutAction()
+      })
+    }
 
     return (
         <nav className={navClasses}>
@@ -67,6 +77,20 @@ export function MenuAdmin() {
               <PlusIcon />
               Criar post
             </Link>
+
+            <a onClick={handleLogout} href="#" className={linkClasses}>
+              {isPending ? (
+                <>
+                  <HourglassIcon />
+                  Aguarde...
+                </>
+              ) : (
+                <>
+                  <LogOutIcon />
+                  Sair
+                </>
+              )}
+            </a>
         </nav>
     )
 }
